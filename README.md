5d31d45e290bb682a7d4c5e5372ea39939c9ff17
Niklas Mollenhauer <nikeee@outlook.com>
2025-02-08 15:06:37 +0100

    Add type defs

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # node-ntag424-sun
 
 Create SUN messages for NTAG 424 DNA using [RFC 6570](https://datatracker.ietf.org/doc/html/rfc6570) URI templates.
+
+## Usage
+```sh
+npm install git+https://github.com/nikeee/node-ntag424-sun.git
+```
---
+++ src/sun.d.ts
@@ -0,0 +1,14 @@
+export type SunNdefMessageData = {
+  buffer: Buffer;
+  offsets: {
+    picc: number | null;
+    uid: number | null;
+    counter: number | null;
+    cmac: number | null;
+  };
+};
+
+/**
+ * @param {string} template [RFC 6570](https://datatracker.ietf.org/doc/html/rfc6570) URI template
+ */
+export function createNdefMessage(template: string): SunNdefMessageData;
